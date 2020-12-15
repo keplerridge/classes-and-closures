39d35af17f02a4340c625533e31bc4ef9dfef517
keplerridge <kepler.ridge@gmail.com>
2020-12-15 09:16:47 -0700

    updated one part of code to be cleaner

--- classes.js
+++ classes.js
@@ -153,7 +153,7 @@ class ProgressiveManager extends Manager {
 */
 
 class Machine {
-  constructor(widgets_made_count, wear_and_tear_count, needs_reboot){
+  constructor(){
     this.widgets_made_count = 0;
     this.wear_and_tear_count = 0;
     this.needs_reboot = false;
